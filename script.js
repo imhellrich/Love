52b60d78c5a9f230fdeb10d8b0160fd3416d71b0
Nagesh Kar <nageshencrypted@gmail.com>
2024-02-29 22:21:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function revealMessage() {
+    var hiddenMessage = document.getElementById('hiddenMessage');
+    hiddenMessage.style.display = 'block';
+    document.getElementById('revealButton').style.display = 'none';
+}
